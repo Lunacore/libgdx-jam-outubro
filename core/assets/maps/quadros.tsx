09bb1895683b5dda5a3e96d922dc77f8376a0ace
titoncio <titoncio@gmail.com>
2018-10-28 18:02:18 -0300

    Adicionando mapas

--- core/assets/maps/quadros.tsx
+++ core/assets/maps/quadros.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="quadros" tilewidth="800" tileheight="600" tilecount="5" columns="0">
+<tileset version="1.2" tiledversion="1.2.0" name="quadros" tilewidth="800" tileheight="600" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="800" height="600" source="quadros/lunalisa.jpg"/>
@@ -15,5 +15,20 @@
  </tile>
  <tile id="4">
   <image width="800" height="600" source="quadros/o-nascimento-de-luna.jpg"/>
+ </tile>
+ <tile id="5">
+  <image width="800" height="600" source="quadros/piet-mondriluna.jpg"/>
+ </tile>
+ <tile id="6">
+  <image width="800" height="600" source="quadros/voice-of-luna.jpg"/>
+ </tile>
+ <tile id="7">
+  <image width="800" height="600" source="quadros/paintinciv.png"/>
+ </tile>
+ <tile id="8">
+  <image width="800" height="600" source="quadros/romero-luna.jpg"/>
+ </tile>
+ <tile id="9">
+  <image width="800" height="600" source="quadros/ecce-homo.jpg"/>
  </tile>
 </tileset>
