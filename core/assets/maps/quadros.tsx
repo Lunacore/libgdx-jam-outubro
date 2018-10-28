2de6d4328254b38e439228f831849445c3e866e9
titoncio <titoncio@gmail.com>
2018-10-28 12:32:09 -0300

    maps + paintings

---
+++ core/assets/maps/quadros.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="quadros" tilewidth="800" tileheight="600" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="800" height="600" source="quadros/lunalisa.jpg"/>
+ </tile>
+ <tile id="1">
+  <image width="800" height="600" source="quadros/o-grito-de-luna.jpg"/>
+ </tile>
+ <tile id="2">
+  <image width="800" height="600" source="quadros/salvador-daluna.jpg"/>
+ </tile>
+ <tile id="3">
+  <image width="800" height="600" source="quadros/a-ultima-ceia-de-luna.jpg"/>
+ </tile>
+ <tile id="4">
+  <image width="800" height="600" source="quadros/o-nascimento-de-luna.jpg"/>
+ </tile>
+</tileset>
