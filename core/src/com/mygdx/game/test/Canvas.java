efcfd00c34e0748c1d81f9109081ac613a4aa9dc
titoncio <titoncio@gmail.com>
2018-10-29 18:47:39 -0300

    levels e refletor espelhado

--- core/src/com/mygdx/game/test/Canvas.java
+++ core/src/com/mygdx/game/test/Canvas.java
@@ -50,7 +50,7 @@ public class Canvas extends GameObject{
 	
 	static float musicCurrentPitch;
 	
-	public static String levelToLoad = "maps/fase10.tmx";
+	public static String levelToLoad = "maps/fase1.tmx";
 	
 	static Sound musicTest;
 	long musicID;
